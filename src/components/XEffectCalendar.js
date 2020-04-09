05398ebe8cb893010dc3b6d9f5979efebfe35a81
Nghia <nphan@fandm.edu>
2020-04-09 16:34:05 -0500

    actually implement functionality to highlight only days with all todos finished

--- src/components/XEffectCalendar.js
+++ src/components/XEffectCalendar.js
@@ -5,10 +5,16 @@ import Calendar from 'react-calendar'
 const XEffectCalendar = () => {
 	const journals = useSelector(state => state.journals)
 	let days = []
-	for (let i = 0; i < journals.length; i++) {
-		const day = new Date(journals[i].date)
-		day.setMinutes(day.getTimezoneOffset())
-		days.push(day)
+	if (journals.length > 0) {
+		journals.map(journal => {
+			const notDoneTodos = journal.todos.filter(todo => !todo.done)
+			console.log(journal.todos, notDoneTodos, notDoneTodos.length === 0)
+			if (notDoneTodos.length === 0) {
+				const day = new Date(journal.date)
+				day.setMinutes(day.getTimezoneOffset())
+				days.push(day)
+			}
+		})
 	}
 	console.log(days)
 
