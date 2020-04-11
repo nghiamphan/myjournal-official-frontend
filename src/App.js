04fedc1a570e15bd7c756829f5ad8f1d72c7fbde
Nghia <nphan@fandm.edu>
2020-04-10 22:24:12 -0500

    use navigation bar with 'react-router-dom'; move loading initial journals from Journals to App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import {
+	BrowserRouter as Router,
+	Switch, Route, Link
+} from 'react-router-dom'
 import XEffectCalendar from './components/XEffectCalendar'
 import Journals from './components/Journals'
 import AddJournal from './components/AddJournal'
+import { initializeJournals } from './reducers/journalReducer'
 
 const App = () => {
 
+	const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(initializeJournals())
+	}, [dispatch])
+
+	const padding = {
+		padding: 5
+	}
+
 	return (
-		<div className="App">
-			<XEffectCalendar/>
-			<Journals/>
-			<AddJournal/>
-		</div>
+		<Router>
+			<div>
+				<Link style={padding} to="/calendar">X Effect Calendar</Link>
+				<Link style={padding} to="/journals">Journals</Link>
+			</div>
+
+			<Switch>
+				<Route path="/calendar">
+					<XEffectCalendar/>
+				</Route>
+				<Route path="/journals">
+					<Journals/>
+					<AddJournal/>
+				</Route>
+			</Switch>
+		</Router>
 	)
 }
 
--- src/components/Journals.js
+++ src/components/Journals.js
@@ -1,15 +1,10 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { initializeJournals } from '../reducers/journalReducer'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import Journal from './Journal'
 
 const Journals = () => {
-	const dispatch = useDispatch()
 	const journals = useSelector(state => state.journals)
 
-	useEffect(() => {
-		dispatch(initializeJournals())
-	}, [dispatch])
 	return (
 		<ul>
 			<h1>My Journal</h1>
