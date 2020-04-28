08acbd46aee4182b3a99925da72a7014e0951029
Nghia <nphan@fandm.edu>
2020-04-28 12:11:16 -0500

    polish datelist

--- src/components/journals/master/DateList.js
+++ src/components/journals/master/DateList.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { setDisplayedJournalId } from '../../../reducers/journalsReducer'
 
 const DateList = () => {
 	const dispatch = useDispatch()
 	const journals = useSelector(state => state.journalsRedux.journals)
+	const displayedJournalId = useSelector(state => state.journalsRedux.displayedJournalId)
 
 	return (
-		<ul>
+		<div className="date-list-page">
 			{journals && journals.length > 0 && journals.map(journal =>
 				<div
 					key={journal.id}
 					onClick={() => dispatch(setDisplayedJournalId(journal.id))}>
-					{journal.date}
+					<div className="h6 date-list-item">
+						{journal.date}&nbsp;
+						{displayedJournalId === journal.id &&
+						<FontAwesomeIcon icon={faCheck}/>}
+					</div>
 				</div>
 			)}
-		</ul>
+		</div>
 	)
 }
 
