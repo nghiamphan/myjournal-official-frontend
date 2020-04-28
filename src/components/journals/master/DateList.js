c8f59c29ee5e905d7ddb83fac60df5a30460c55a
Nghia <nphan@fandm.edu>
2020-04-28 17:51:06 -0500

    update/fix the indicator next to the date in datelist that shows which date is currently selected

--- src/components/journals/master/DateList.js
+++ src/components/journals/master/DateList.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 import { setDisplayedJournalId } from '../../../reducers/journalsReducer'
 
 const DateList = () => {
 	const dispatch = useDispatch()
 	const journals = useSelector(state => state.journalsRedux.journals)
 	const displayedJournalId = useSelector(state => state.journalsRedux.displayedJournalId)
+	const journalToUpdateId = useSelector(state => state.journalsRedux.journalToUpdateId)
 
 	return (
 		<div className="date-list-page">
@@ -17,8 +18,8 @@ const DateList = () => {
 					onClick={() => dispatch(setDisplayedJournalId(journal.id))}>
 					<div className="h6 date-list-item">
 						{journal.date}&nbsp;
-						{displayedJournalId === journal.id &&
-						<FontAwesomeIcon icon={faCheck}/>}
+						{(journal.id === displayedJournalId || journal.id === journalToUpdateId )&&
+						<FontAwesomeIcon icon={faEllipsisV}/>}
 					</div>
 				</div>
 			)}
